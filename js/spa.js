31ab4d14d7da685edca23db646c92240da26ce09
Roman Gural <hcooh4u@gmail.com>
2014-04-18 18:01:49 +0200

    finished 3.4

--- js/spa.js
+++ js/spa.js
@@ -12,9 +12,7 @@ sloppy:true, vars: false, white: true
 
 var spa = (function() {
     var initModule = function ($container) {
-        $container.html('<h1 style="display:inline-block; margin:25px;">' +
-            'hello world!' +
-            '</h1>')
+        spa.shell.initModule($container);
     };
     return {
         initModule: initModule
--- js/spa.shell.js
+++ js/spa.shell.js
@@ -1,3 +1,59 @@
 /**
  * Created by silvan on 4/16/14.
+ * spa.shell.js
+ * Root namespace module
  */
+
+/*jslint    browser : true, continue:true, devel: true, indent : 2
+ maxerr:50, newcap: true, nomen : true, plusplus: true, regexp:true,
+ sloppy:true, vars: false, white: true
+ */
+/* global $, spa */
+
+spa.shell = (function () {
+    // begin module scope variables
+    var
+        configMap = {
+            main_html: String()
+                +'<div class="spa-shell-head">'
+                + '<div class="spa-shell-head-logo"></div>'
+                + '<div class="spa-shell-head-acct"></div>'
+                + '<div class="spa-shell-head-search"></div>'
+                + '</div>'
+                + '<div class="spa-shell-main">'
+                + '<div class="spa-shell-main-nav"></div>'
+                + '<div class="spa-shell-main-content"></div>'
+                + '</div>'
+                + '<div class="spa-shell-foot"></div>'
+                + '<div class="spa-shell-chat"></div>'
+                + '<div class="spa-shell-modal"></div>'
+        },
+        stateMap = {$container : null},
+        jqueryMap = {},
+        setJqueryMap, initModule;
+    // end module scope variables
+    // begin utility methods
+    // end utility methods
+    // begin dom methods
+    // Begin DOM method /setJqueryMap/
+    setJqueryMap = function () {
+        var $container = stateMap.$container;
+        jqueryMap = { $container : $container };
+    };
+    // End DOM method /setJqueryMap/
+    // end dom methods
+
+    // begin event handlers
+    // end event handlers
+
+    // begin public methods
+    // begin public method /initModule/
+    initModule = function ($container) {
+        stateMap.$container = $container;
+        $container.html(configMap.main_html);
+        setJqueryMap();
+    };
+    // end public method /initModule/
+
+    return {initModule: initModule};
+}());
